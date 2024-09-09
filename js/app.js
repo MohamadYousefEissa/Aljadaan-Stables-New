fab044813c3c0cdd39600837744aa79939f3c5e4
mye7 <yousefessa810@gmail.com>
2024-09-09 18:19:36 +0300

    fix mobile nav & fix gallery & change mobile nav animation

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@ const nav = gsap.timeline({ paused: true, duration: 0.1 });
 const hamburger = document.querySelector(".hamburger input");
 
 nav.from("#mobile-menu", {
-  left: "40%",
+  // left: "50%",
   opacity: 0,
-  duration: 0.3,
+  duration: 0.15,
+  display: "none",
 });
 nav.from("#mobile-menu li", {
   x: 100,
