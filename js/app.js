1d8e1f40ac3b0537d4de5a47f026cf5de4367a4e
mye7 <yousefessa810@gmail.com>
2024-09-11 02:30:35 +0300

    improve mobile menu animation & edit footer layout

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ const nav = gsap.timeline({ paused: true, duration: 0.1 });
 const hamburger = document.querySelector(".hamburger input");
 
 nav.from("#mobile-menu", {
-  // left: "50%",
   opacity: 0,
   duration: 0.15,
   display: "none",
@@ -10,9 +9,36 @@ nav.from("#mobile-menu", {
 nav.from("#mobile-menu li", {
   x: 100,
   opacity: 0,
-  stagger: 0.1,
+  stagger: 0.05,
 });
 hamburger.addEventListener("change", (ev) => {
   if (!ev.target.checked) nav.reverse();
   else nav.play();
 });
+
+// const form = document.querySelector("form");
+// const nameInput = form.querySelector("input[name='name']");
+// const emailInput = form.querySelector("input[name='email']");
+// const messageInput = form.querySelector("textarea[name='message']");
+// const button = form.querySelector("button");
+// form.addEventListener("submit", (ev) => {
+//   ev.preventDefault();
+//   if (
+//     nameInput.value.length < 30 &&
+//     nameInput.value.length >= 3 &&
+//     emailInput.value.length < 40 &&
+//     emailInput.value.length >= 3 &&
+//     messageInput.value.length < 1000 &&
+//     messageInput.value.length >= 3
+//   ) {
+//     nameInput.value = "";
+//     emailInput.value = "";
+//     messageInput.value = "";
+//     button.innerText = "Send Successfully";
+//     button.classList.add("bg-success");
+//     setTimeout(() => {
+//       button.innerText = "Send";
+//       button.classList.remove("bg-success");
+//     }, 2000);
+//   }
+// });
