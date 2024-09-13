6546bfb23b9dbd94f96b5d967e549dc928b50a50
mye7 <yousefessa810@gmail.com>
2024-09-14 02:57:52 +0300

    create contactus page & fix footer & remove unused fonts & create layouts folder

---
+++ layouts/footer.js
@@ -0,0 +1,56 @@
+const footerContent = `
+      <div class="container p-5 pb-3 pb-md-5 px-md-0 px-lg-5">
+        <div class="row justify-content-center align-items-center">
+          <div class="col-12 col-md-4 d-flex d-md-block justify-content-center">
+            <img
+              src="./images/main-logo.svg"
+              alt="Logo"
+              class="img-fluid"
+              id="footer-logo"
+            />
+          </div>
+
+          <div
+            class="col-12 col-md-6 col-lg-5 row row-cols-2 row-cols-md-3 mt-5 mt-md-0 text-center text-md-start text-nowrap"
+          >
+            <div class="d-flex flex-column gap-2 gap-md-0">
+              <a href="/index.html">Home</a>
+              <a href="">About us</a>
+              <a href="">Syria</a>
+            </div>
+            <div class="d-flex flex-column gap-2 gap-md-0">
+              <a href="">Partnership</a>
+              <a href="">Stallions</a>
+              <a href="">Mares</a>
+            </div>
+            <div
+              class="d-flex flex-row flex-md-column gap-4 gap-md-0 flex-grow-1 mt-2 mt-md-0"
+            >
+              <a href="" class="last-two-anchors">Offsprings</a>
+              <a href="/contactus.html" class="last-two-anchors">Contact us</a>
+            </div>
+          </div>
+          <div
+            class="col d-flex gap-4 justify-content-center justify-content-lg-end mt-5 mt-md-0"
+          >
+            <a href="">
+              <img
+                src="./icons/facebook.svg"
+                alt="facebbok"
+                class="img-fluid"
+              />
+            </a>
+            <a href="https://www.instagram.com/aljadaanstables" target="_blank">
+              <img
+                src="./icons/instagram.svg"
+                alt="instagram"
+                class="img-fluid"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+`;
+const footer = document.createElement("footer");
+footer.innerHTML = footerContent;
+main.insertAdjacentElement("afterend", footer);
---
+++ layouts/header.js
@@ -0,0 +1,47 @@
+const headerContent = `
+      <nav>
+        <div class="container">
+          <div class="row">
+            <div
+              class="col d-flex flex-row flex-md-column flex-lg-row justify-content-between align-items-center py-3 px-2 px-xl-5"
+            >
+              <a href="/index.html"
+                ><img src="./images/main-logo.svg" alt="Logo" class="img-fluid"
+              /></a>
+              <label class="hamburger d-md-none">
+                <input type="checkbox" />
+                <svg viewBox="0 0 32 32">
+                  <path
+                    class="line line-top-bottom"
+                    d="M27 10 13 10C10.8 10 9 8.2 9 6 9 3.5 10.8 2 13 2 15.2 2 17 3.8 17 6L17 26C17 28.2 18.8 30 21 30 23.2 30 25 28.2 25 26 25 23.8 23.2 22 21 22L7 22"
+                  ></path>
+                  <path class="line" d="M7 16 27 16"></path>
+                </svg>
+              </label>
+              <ul
+                class="d-none d-md-flex align-items-end h-100 gap-4 gap-lg-5 mt-md-3 mt-lg-0 p-0"
+              >
+                <li><a href="">About us</a></li>
+                <li><a href="">The indigenous breed</a></li>
+                <li><a href="">Stallions</a></li>
+                <li><a href="">Mares</a></li>
+                <li><a href="">Offsprings</a></li>
+                <li><a href="/contactus.html">Contact us</a></li>
+              </ul>
+              <ul class="d-md-none p-0" id="mobile-menu">
+                <li><a href="">About us</a></li>
+                <li><a href="">The indigenous breed</a></li>
+                <li><a href="">Stallions</a></li>
+                <li><a href="">Mares</a></li>
+                <li><a href="">Offsprings</a></li>
+                <li><a href="/contactus.html">Contact us</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+`;
+const main = document.querySelector("main");
+const header = document.createElement("header");
+header.innerHTML = headerContent;
+main.insertAdjacentElement("beforebegin", header);
